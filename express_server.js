54a401de8917c957b0ca2e0570b49093c7b084ee
nvTwistt <mtamkee@icloud.com>
2021-06-07 17:01:22 +0000

    adding

--- express_server.js
+++ express_server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.set("view engine", "ejs");
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -20,3 +25,12 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+
+}
