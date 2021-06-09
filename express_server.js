da979bb7341d7a427eeebca8874c89a38dbd11ee
nvTwistt <mtamkee@icloud.com>
2021-06-09 16:54:41 +0000

    commit

--- express_server.js
+++ express_server.js
@@ -2,7 +2,21 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -48,3 +62,29 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[cleanURL];
   res.redirect(longURL);
 });
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  let deleteURL = req.params.shortURL;
+  delete urlDatabase[deleteURL];
+  res.redirect(302, "/urls");
+})
+
+app.post("/urls/:shortURL/edit", (req, res) => {
+  let redirectURL = req.params.shortURL;
+  res.redirect(302, `/urls/${redirectURL}`);
+})
+
+app.post("/urls/:shortURL/submit", (req, res) => {
+  let id = req.params.shortURL;
+  let newURL = req.body.newURL;
+  urlDatabase[id] = newURL;
+  res.redirect(302, "/urls");
+})
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  response.cookie('username', `${username}`);
+  response.redirect('/urls');
+});
+
+
