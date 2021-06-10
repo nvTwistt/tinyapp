1f5381f3d0fe1d427a8aede072ba35d4492d1c59
mtamkee <33907017+mtamkee@users.noreply.github.com>
2021-06-10 12:11:36 -0600

    adding tests

---
+++ test/helperTest.js
@@ -0,0 +1,41 @@
+const { assert } = require('chai');
+
+const { emailFinder } = require('../helper.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('emailFinder', function() {
+  it('should return a user with valid email', function() {
+    const user = emailFinder("user@example.com", users)
+    const expectedOutput = "userRandomID";
+    // Write your assert statement here
+    assert.strictEqual(user, expectedOutput);
+  });
+});
+describe('emailFinder', function() {
+    it('should return a user with valid email', function() {
+      const user = emailFinder("user@example.com", users)
+      const expectedOutput = "userRandomID";
+      // Write your assert statement here
+      assert.strictEqual(user, expectedOutput);
+    });
+});
+describe('emailFiner', function() {
+    it('should return undefined', function() {
+      const user = getUserByEmail("", users)
+      const expectedOutput = undefined;
+      // Write your assert statement here
+      assert.strictEqual(user, expectedOutput);
+    });
+  });
